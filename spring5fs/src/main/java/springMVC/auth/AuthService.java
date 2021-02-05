9ef121fd6f3b00e59d144dc8da6d7a650ba0c275
exit5710 <exit5710@gmail.com>
2021-02-05 21:45:02 +0900

    spring5fs springMVC

---
+++ spring5fs/src/main/java/springMVC/auth/AuthService.java
@@ -0,0 +1,27 @@
+package springMVC.auth;
+
+import common.exception.WrongIdPasswordException;
+
+import springMVC.member.Member;
+import springMVC.member.MemberDao;
+
+public class AuthService {
+	private MemberDao memberDao;
+
+	public void setMemberDao(MemberDao memberDao) {
+		this.memberDao = memberDao;
+	}
+
+	public AuthInfo authenticate(String email, String password) {
+		Member member = memberDao.selectByEmail(email);
+		if (member == null) {
+			throw new WrongIdPasswordException();
+		}
+
+		if (!member.matchPassword(password)) {
+			throw new WrongIdPasswordException();
+		}
+
+		return new AuthInfo(member.getId(), member.getEmail(), member.getName());
+	}
+}
