72cc3722ff65f5df17ac0479d200a8880231ee62
exit5710 <exit5710@gmail.com>
2021-02-10 22:18:32 +0900

    spring5fs springMVC

--- spring5fs/src/main/java/springMVC/config/MvcConfig.java
+++ spring5fs/src/main/java/springMVC/config/MvcConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.config.annotation.*;
+import springMVC.interceptor.AuthCheckInterceptor;
 
 @Configuration
 @EnableWebMvc
@@ -31,5 +32,10 @@ public class MvcConfig implements WebMvcConfigurer {
 		messageSource.setDefaultEncoding("UTF-8");
 
 		return messageSource;
+	}
+
+	@Override
+	public void addInterceptors(InterceptorRegistry interceptorRegistry) {
+		interceptorRegistry.addInterceptor(new AuthCheckInterceptor()).addPathPatterns("/edit/**");
 	}
 }
--- spring5fs/src/main/webapp/WEB-INF/views/edit/changePassword.jsp
+++ spring5fs/src/main/webapp/WEB-INF/views/edit/changePassword.jsp
@@ -6,12 +6,17 @@
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix="spring" uri="http://www.springframework.org/tags" %>
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Title</title>
+	<title><spring:message code="change.password.title" /></title>
 </head>
 
 <body>
+	<p>
+		<a href="<c:url value='/main' />">[<spring:message code="go.main" />]</a>
+	</p>
 </body>
 </html>
