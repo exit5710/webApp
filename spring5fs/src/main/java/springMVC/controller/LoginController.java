29123d817810a0154bac27830783f708344db752
exit5710 <exit5710@gmail.com>
2021-02-11 19:15:27 +0900

    spring5fs springMVC

--- spring5fs/src/main/java/springMVC/controller/LoginController.java
+++ spring5fs/src/main/java/springMVC/controller/LoginController.java
@@ -4,6 +4,7 @@ import common.exception.WrongIdPasswordException;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +14,8 @@ import springMVC.auth.AuthService;
 import springMVC.auth.LoginCommandValidator;
 import springMVC.command.LoginCommand;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 @Controller
@@ -25,12 +28,17 @@ public class LoginController {
 	}
 
 	@GetMapping
-	public String form(LoginCommand loginCommand) {
+	public String form(LoginCommand loginCommand, @CookieValue(value = "rememberEmail", required = false) Cookie cookie) {
+		if (cookie != null) {
+			loginCommand.setEmail(cookie.getValue());
+			loginCommand.setRememberEmail(true);
+		}
+
 		return "login/loginForm";
 	}
 
 	@PostMapping
-	public String submit(LoginCommand loginCommand, Errors errors, HttpSession heHttpSession) {
+	public String submit(LoginCommand loginCommand, Errors errors, HttpSession heHttpSession, HttpServletResponse response) {
 		new LoginCommandValidator().validate(loginCommand, errors);
 		if (errors.hasErrors()) {
 			return "login/loginForm";
@@ -39,6 +47,17 @@ public class LoginController {
 		try {
 			AuthInfo authInfo = authService.authenticate(loginCommand.getEmail(), loginCommand.getPassword());
 			heHttpSession.setAttribute("authInfo", authInfo);
+
+			Cookie cookie = new Cookie("rememberEmail", loginCommand.getEmail());
+			cookie.setPath("/");
+			if (loginCommand.isRememberEmail()) {
+				cookie.setMaxAge(60 * 60 * 24 * 30);
+			} else {
+				cookie.setMaxAge(0);
+			}
+
+			response.addCookie(cookie);
+
 			return "login/loginSuccess";
 		} catch (WrongIdPasswordException e) {
 			errors.reject("idPasswordNotMatching");
