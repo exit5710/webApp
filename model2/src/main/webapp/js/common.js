e078e4324773ebdbb24d40691f3b8e8df619b7ea
exit5710 <exit5710@gmail.com>
2023-07-09 18:44:15 +0900

    model2 common

--- model2/src/main/webapp/js/common.js
+++ model2/src/main/webapp/js/common.js
@@ -60,19 +60,3 @@ $.fn.serializeObject = function () {
 	//return JSON.stringify(result); // return result;
 	return JSON.parse(JSON.stringify(result));
 };
-
-const fn_tabSetting = function () {
-	$('.tabcontent > div').hide();
-	$('.idTabs a').click(function () {
-		$('.tabcontent > div').hide().filter(this.hash).fadeIn();
-		$('.idTabs a').removeClass('act');
-		$(this).addClass('act');
-
-		try {
-			fn_tabClickCallBack($(this)[0].hash);
-		} catch {
-		}
-
-		return false;
-	}).filter(':eq(0)').click();
-};
