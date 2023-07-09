5e915842caca27a64b427d8ea65be065b885454e
exit5710 <exit5710@gmail.com>
2023-07-09 18:43:25 +0900

    model2 generalTab

---
+++ model2/src/main/webapp/js/generalTab.js
@@ -0,0 +1,17 @@
+"use strict";
+
+const fn_tabSetting = function () {
+	$('.tabcontent > div').hide();
+	$('.idTabs a').click(function () {
+		$('.tabcontent > div').hide().filter(this.hash).fadeIn();
+		$('.idTabs a').removeClass('act');
+		$(this).addClass('act');
+
+		try {
+			fn_tabClickCallBack($(this)[0].hash);
+		} catch {
+		}
+
+		return false;
+	}).filter(':eq(0)').click();
+};
