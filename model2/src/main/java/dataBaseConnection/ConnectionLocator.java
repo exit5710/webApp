5bba343668a580a4eef5a48ede608cfbdc1a0e04
exit5710 <exit5710@gmail.com>
2023-07-11 22:05:00 +0900

    model2 ConnectionLocator

--- model2/src/main/java/dataBaseConnection/ConnectionLocator.java
+++ model2/src/main/java/dataBaseConnection/ConnectionLocator.java
@@ -1,7 +1,5 @@
 package dataBaseConnection;
 
-import javax.servlet.http.HttpServletRequest;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
