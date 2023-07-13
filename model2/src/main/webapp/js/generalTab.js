bd271c0a373ab63b22d4bf6902eae8a8147386f4
exit5710 <exit5710@gmail.com>
2023-07-13 15:47:26 +0900

    model2 generalTab

--- model2/src/main/webapp/js/generalTab.js
+++ model2/src/main/webapp/js/generalTab.js
@@ -2,17 +2,26 @@
 // generalTab
 
 const fn_tabSetting = function () {
-	$('.tabcontent > div').hide();
-	$('.idTabs a').click(function () {
-		$('.tabcontent > div').hide().filter(this.hash).fadeIn();
-		$('.idTabs a').removeClass('act');
-		$(this).addClass('act');
+	let currentTab = "";
+
+	$(".tabcontent > div").hide();
+	$(".idTabs a").click(function () {
+		if (currentTab === $(this)[0].hash) {
+			return false;
+		}
+
+		currentTab = $(this)[0].hash;
+
+		$(".tabcontent > div").hide().filter(this.hash).fadeIn();
+		$(".idTabs a").removeClass("act");
+		$(this).addClass("act");
 
 		try {
-			fn_tabClickCallBack($(this)[0].hash);
-		} catch {
+			fn_tabClickCallBack(currentTab);
+		} catch (error) {
+			console.error(error);
 		}
 
 		return false;
-	}).filter(':eq(0)').click();
+	}).filter(":eq(0)").click();
 };
