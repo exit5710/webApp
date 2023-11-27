524c1e8b6f2a11796836dff68154ce132640d89b
exit5710 <exit5710@gmail.com>
2023-11-27 20:37:15 +0900

    model2 RacerListAction

--- model2/src/main/java/racers/action/RacerListAction.java
+++ model2/src/main/java/racers/action/RacerListAction.java
@@ -3,6 +3,7 @@ package racers.action;
 import com.google.gson.Gson;
 import controller.Action;
 import controller.ActionForward;
+import customException.CustomException;
 import dataBaseConnection.ConnectionLocator;
 import dataBaseConnection.MariaDataBase;
 import org.slf4j.Logger;
@@ -12,21 +13,22 @@ import racers.vo.RacerVo;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class RacerListAction implements Action {
 	private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
 	@Override
-	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws SQLException, IOException {
 		logger.info("RacerListAction /");
 
 		ConnectionLocator connectionLocator = MariaDataBase.getInstance();
 		RacerDao racerDao = RacerDao.getInstance(connectionLocator);
 
 		ArrayList<RacerVo> arrayList = racerDao.selectRacer();
-
 		Gson gson = new Gson();
 		String jsonString = gson.toJson(arrayList);
 
@@ -36,6 +38,6 @@ public class RacerListAction implements Action {
 		out.flush();
 		out.close();
 
-		return null;
+		throw new CustomException("json");
 	}
 }
