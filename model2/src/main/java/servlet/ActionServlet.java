cad3ff96c251bf5207c4521c106e6a78318835ca
exit5710 <exit5710@gmail.com>
2023-12-25 00:29:04 +0900

    model2 ActionServlet

--- model2/src/main/java/servlet/ActionServlet.java
+++ model2/src/main/java/servlet/ActionServlet.java
@@ -3,10 +3,15 @@ package servlet;
 import controller.Action;
 import controller.ActionForward;
 import controller.UserActionFactory;
+
+import customException.CustomException;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -14,12 +19,18 @@ import javax.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
-public class ActionServlet extends HttpServlet {
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import java.util.Enumeration;
+
+public class ActionServlet extends HttpServlet implements ServletContextListener {
 	private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
 	@Override
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		//logger.info("model2 ActionServlet /");
+		logger.debug("model2 ActionServlet.service /");
 
 		request.setCharacterEncoding("UTF-8");
 		response.setCharacterEncoding("UTF-8");
@@ -48,16 +59,40 @@ public class ActionServlet extends HttpServlet {
 					RequestDispatcher rd = request.getRequestDispatcher(actionForward.getPath());
 					rd.forward(request, response);
 				}
-			} catch (Exception e) {
+			} catch (SQLException | NullPointerException e) {
 				// TODO: handle exception
 				// json, 파일 다운로드는 리턴값이 null
-				//logger.error("message", e);
-				logger.error(e.toString());
+				logger.error("message : " + e.toString());
+			} catch (CustomException e) {
+				logger.debug(e.toString());
+			} catch (Exception e) {
+				logger.error("exception : " + e.toString());
 			}
 		} else {
 			// 유효하지 않는 command는 404error.jsp로 이동
 			RequestDispatcher rd = request.getRequestDispatcher("WEB-INF/views/error/404error.jsp");
 			rd.forward(request, response);
+		}
+	}
+
+	@Override
+	public void contextDestroyed(ServletContextEvent servletContextEvent) {
+		logger.info("model2 ActionServlet.contextDestroyed /");
+
+		try {
+			Enumeration<Driver> drivers = DriverManager.getDrivers();
+
+			while (drivers.hasMoreElements()) {
+				Driver driver = drivers.nextElement();
+				DriverManager.deregisterDriver(driver);
+				/*
+				if (driver instanceof org.mariadb.jdbc.Driver || driver instanceof net.sf.log4jdbc.sql.jdbcapi.DriverSpy) {
+					DriverManager.deregisterDriver(driver);
+				}
+				*/
+			}
+		} catch (SQLException e) {
+			logger.error("exception : " + e.toString());
 		}
 	}
 }
