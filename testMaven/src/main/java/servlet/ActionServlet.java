14f519179b5b464347b7681874bf42726496c1db
exit5710 <exit5710@gmail.com>
2024-01-15 00:03:10 +0900

    testMaven ActionServlet

--- testMaven/src/main/java/servlet/ActionServlet.java
+++ testMaven/src/main/java/servlet/ActionServlet.java
@@ -3,11 +3,9 @@ package servlet;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import java.io.IOException;
 
 public class ActionServlet extends HttpServlet {
